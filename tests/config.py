26c625118b2bc102f2d6bbc0d50b652bf65ba033
evgfilim1 <evgfilim1@yandex.ru>
2024-07-31 00:27:35 +0500

    Check if environment variables are set, add defaults

--- tests/config.py
+++ tests/config.py
@@ -1,11 +1,34 @@
 import os
 from dataclasses import dataclass
-from typing import Self, Literal
+from pathlib import Path
+from typing import Self, Literal, overload, NewType, cast
+
+_Required = NewType("_Required", object)
+_required = _Required(object())
+
+
+@overload
+def env(key: str, default: _Required = _required) -> str: ...
+
+
+@overload
+def env(key: str, default: str) -> str: ...
+
+
+@overload
+def env(key: str, default: None) -> str | None: ...
+
+
+def env(key: str, default: str | _Required | None = _required) -> str | None:
+    value = os.getenv(key, None)
+    if value is None and default is _required:
+        raise ValueError(f"Required environment variable {key!r} is not set")
+    return value if value is not None else default
 
 
 @dataclass(kw_only=True)
 class Config:
-    downloads_dir: Literal["{tempdir}"] | str
+    downloads_dir: Literal["{tempdir}"] | Path
     selenium_hub_url: str
     current_region_substr: str
     default_wait_timeout: int
@@ -13,11 +36,16 @@ class Config:
 
     @classmethod
     def from_env(cls) -> Self:
-        env = os.getenv
+        downloads_dir_raw = env("DOWNLOADS_DIR", "{tempdir}")
+        downloads_dir: Literal["{tempdir}"] | Path
+        if downloads_dir_raw == "{tempdir}":
+            downloads_dir = cast(Literal["{tempdir}"], downloads_dir_raw)
+        else:
+            downloads_dir = Path(downloads_dir_raw)
         return cls(
-            downloads_dir=env("DOWNLOADS_DIR"),
+            downloads_dir=downloads_dir,
             selenium_hub_url=env("SELENIUM_HUB_URL"),
             current_region_substr=env("CURRENT_REGION_SUBSTR"),
-            default_wait_timeout=int(env("DEFAULT_WAIT_TIMEOUT")),
+            default_wait_timeout=int(env("DEFAULT_WAIT_TIMEOUT", "15")),
             record_videos=env("RECORD_VIDEOS", "").lower() not in {"0", "false", ""},
         )
